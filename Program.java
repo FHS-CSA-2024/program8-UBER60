714debe7b2a7cc601c48004f2065b47734963e6f
UBER60 <rcho5318@gmail.com>
2024-08-26 15:05:16 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 //import stuff here
 import java.util.Scanner;
 //Your code here
-public class Program8{
+public class Program{
   public static void main(String[] args){
     Scanner x = new Scanner(System.in);
     System.out.print("Enter number 1: ");
